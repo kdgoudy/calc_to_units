e9447196bb36a83e9aa4b567dc0498f91c2a15cd
Koki <kgoudy64@gmail.com>
2022-04-02 11:12:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 calculation_to_units = 24
 name_of_units = "hours"
 
-print(f"20 days are {20 * calculation_to_units} {name_of_units}")
-print(f"35 days are {35 * calculation_to_units} {name_of_units}")
-print(f"50 days are {50 * calculation_to_units} {name_of_units}")
-print(f"110 days are {110 * calculation_to_units} {name_of_units}")
+def days_to_units(num_of_days):
+  print(f"{num_of_days} day are {num_of_days * calculation_to_units} {name_of_units}")
+
+days_to_units(20)
+days_to_units(35)
+days_to_units(50)
+days_to_units(110)
