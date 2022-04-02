a02fea03fb08667f7afcebdd69a1514eb132ad96
Koki <kgoudy64@gmail.com>
2022-04-02 11:07:35 -0500

    Create main.py
    Multiplying hours of days x number of days

---
+++ main.py
@@ -0,0 +1,8 @@
+#Creating variables to measure hours in time of days
+calculation_to_units = 24
+name_of_units = "hours"
+
+print(f"20 days are {20 * calculation_to_units} {name_of_units}")
+print(f"35 days are {35 * calculation_to_units} {name_of_units}")
+print(f"50 days are {50 * calculation_to_units} {name_of_units}")
+print(f"110 days are {110 * calculation_to_units} {name_of_units}")
